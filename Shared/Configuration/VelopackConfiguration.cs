000b8cdd008cbcddf790d749facbdeff076959c7
yassin-ajanif <y.ajanif@enjazdev.com>
2026-07-22 19:27:37 +0200

    Update project branding to "Ecomati société" in various files, including project configuration, UI translations, and release scripts.

--- Shared/Configuration/VelopackConfiguration.cs
+++ Shared/Configuration/VelopackConfiguration.cs
@@ -4,5 +4,5 @@ public static class VelopackConfiguration
 {
     public const string PackId = "Ecomati";
     public const string MainExe = "GestionCommerciale.exe";
-    public const string GitHubRepoUrl = "https://github.com/yassin-ajanif/Ecomati";
+    public const string GitHubRepoUrl = "https://github.com/yassin-ajanif/EcomatiSoci-t-";
 }
